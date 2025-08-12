b67002722a35ba521acb4b9e3fdcd3f1e51ed454
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-08-13 01:51:31 +0300

    feat(backend):init backend

---
+++ backend/src/main/java/org/example/scrap/ScrapApplication.java
@@ -0,0 +1,11 @@
+package org.example.scrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScrapApplication.class, args);
+    }
+}
