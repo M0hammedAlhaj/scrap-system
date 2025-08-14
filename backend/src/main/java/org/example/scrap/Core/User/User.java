9bd9ceaed1d3df6bc0cafd93b8a90b7ff8be89c9
M0hammedAlhaj <2232797@std.hu.edu.jo>
2025-08-14 11:50:05 +0300

    feat(backend):add id to user

--- backend/src/main/java/org/example/scrap/Core/User/User.java
+++ backend/src/main/java/org/example/scrap/Core/User/User.java
@@ -1,10 +1,14 @@
 package org.example.scrap.Core.User;
 
-import org.example.scrap.Core.shared.BaseDocument;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
 public abstract class User {
+
+    @Id
+    private String id;
+
     private String email;
 
     private String password;
@@ -13,40 +17,3 @@ public abstract class User {
 
     private String phoneNumber;
 }
-
-/*
-    User{
-    name:Mohammed,
-    phone:078
-    type:seller
-        Product{
-           {
-           name:laptop,
-           initPrice:10$,
-                Bider{
-                {bider_id:1,
-                price:20},
-                {bider_id:2,
-                price:30
-                }
-           "}
-    }
-        User{
-    name:Mohammed,
-    phone:078
-    type:bayer
-        Product{
-           {
-           name:laptop,
-           initPrice:10$,
-           finalPrice:20$
-                Bider{
-                {bider_id:1,
-                price:20},
-                {bider_id:2,
-                price:30
-                }
-           "}
-    }
-
- */
